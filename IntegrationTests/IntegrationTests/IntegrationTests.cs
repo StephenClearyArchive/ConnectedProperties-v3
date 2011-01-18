fb2e0a9ba6e2aca59b11417fef8b025f1d7c24bd
Stephen Cleary <StephenCleary@users.noreply.github.com>
2011-01-18 14:05:43 -0500

    Added integration test stubs.

---
+++ IntegrationTests/IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod]
+        public void Net4Client_WithCC_HasCCExceptionType()
+        {
+            var assembly = Assembly.LoadFrom(@"..\..\..\Net 4 Client CC\bin\Debug\Net 4 Client CC.exe");
+            var method = assembly.GetType("IntegrationTest.Program").GetMethod("PreconditionViolationExceptionType", BindingFlags.Static | BindingFlags.Public);
+            string result = (string)method.Invoke(null, null);
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void Net4Client_WithoutCC_HasNonCCExceptionType()
+        {
+            var assembly = Assembly.LoadFrom(@"..\..\..\Net 4 Client NoCC\bin\Debug\Net 4 Client NoCC.exe");
+            var method = assembly.GetType("IntegrationTest.Program").GetMethod("PreconditionViolationExceptionType", BindingFlags.Static | BindingFlags.Public);
+            string result = (string)method.Invoke(null, null);
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void Net4Full_WithCC_HasCCExceptionType()
+        {
+            var assembly = Assembly.LoadFrom(@"..\..\..\Net 4 Full CC\bin\Debug\Net 4 Full CC.exe");
+            var method = assembly.GetType("IntegrationTest.Program").GetMethod("PreconditionViolationExceptionType", BindingFlags.Static | BindingFlags.Public);
+            string result = (string)method.Invoke(null, null);
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void Net4Full_WithoutCC_HasNonCCExceptionType()
+        {
+            var assembly = Assembly.LoadFrom(@"..\..\..\Net 4 Full NoCC\bin\Debug\Net 4 Full NoCC.exe");
+            var method = assembly.GetType("IntegrationTest.Program").GetMethod("PreconditionViolationExceptionType", BindingFlags.Static | BindingFlags.Public);
+            string result = (string)method.Invoke(null, null);
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void SL4_WithCC_HasCCExceptionType()
+        {
+            var assembly = Assembly.LoadFrom(@"..\..\..\SL 4 CC\bin\Debug\SL 4 CC.dll");
+            var method = assembly.GetType("IntegrationTest.Program").GetMethod("PreconditionViolationExceptionType", BindingFlags.Static | BindingFlags.Public);
+            string result = (string)method.Invoke(null, null);
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void SL4_WithoutCC_HasNonCCExceptionType()
+        {
+            var assembly = Assembly.LoadFrom(@"..\..\..\SL 4 NoCC\bin\Debug\SL 4 NoCC.dll");
+            var method = assembly.GetType("IntegrationTest.Program").GetMethod("PreconditionViolationExceptionType", BindingFlags.Static | BindingFlags.Public);
+            string result = (string)method.Invoke(null, null);
+            Assert.AreEqual(string.Empty, result);
+        }
+    }
+}
---
+++ IntegrationTests/SL 4 NoCC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SL_4_NoCC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microline Technology Corporation")]
+[assembly: AssemblyProduct("SL_4_NoCC")]
+[assembly: AssemblyCopyright("Copyright © Microline Technology Corporation 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("990c5a05-df57-491a-b914-4fd0fd36702f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
