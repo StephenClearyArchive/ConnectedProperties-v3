240957b22faac7d396061bfe0119adce275f56f3
Stephen Cleary <Stephen Cleary@localhost>
2011-01-18 14:05:43 -0500

    Added integration test stubs.

---
+++ IntegrationTests/Net 4 Client CC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegrationTest
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string PreconditionViolationExceptionType()
+        {
+            return string.Empty;
+        }
+    }
+}
