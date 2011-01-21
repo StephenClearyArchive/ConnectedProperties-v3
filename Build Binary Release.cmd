7278441d2e736331aad93815ec0a13f512ff3eeb
Stephen Cleary <Stephen Cleary@localhost>
2011-01-20 21:32:29 -0500

    Modified build scripts to work with SHFB.

--- Build Binary Release.cmd
+++ Build Binary Release.cmd
@@ -13,19 +13,21 @@ if not exist Binaries\ConnectedProperties\SL4\CodeContracts mkdir Binaries\Conne
 
 copy /Y Source\ConnectedProperties\bin\Debug\ConnectedProperties.dll Binaries\ConnectedProperties\Net40\Debug
 copy /Y Source\ConnectedProperties\bin\Debug\ConnectedProperties.pdb Binaries\ConnectedProperties\Net40\Debug
+copy /Y Help\current\Help\ConnectedProperties.xml Binaries\ConnectedProperties\Net40\Debug
 
 copy /Y Source\ConnectedProperties\bin\Release\ConnectedProperties.dll Binaries\ConnectedProperties\Net40
 copy /Y Source\ConnectedProperties\bin\Release\ConnectedProperties.pdb Binaries\ConnectedProperties\Net40
-copy /Y Source\ConnectedProperties\bin\Release\ConnectedProperties.xml Binaries\ConnectedProperties\Net40
+copy /Y Help\current\Help\ConnectedProperties.xml Binaries\ConnectedProperties\Net40
 copy /Y Source\ConnectedProperties\bin\Release\CodeContracts\ConnectedProperties.Contracts.dll Binaries\ConnectedProperties\Net40\CodeContracts
 copy /Y Source\ConnectedProperties\bin\Release\CodeContracts\ConnectedProperties.Contracts.pdb Binaries\ConnectedProperties\Net40\CodeContracts
 
 copy /Y "Source\ConnectedProperties (SL)\bin\Debug\ConnectedProperties.dll" Binaries\ConnectedProperties\SL4\Debug
 copy /Y "Source\ConnectedProperties (SL)\bin\Debug\ConnectedProperties.pdb" Binaries\ConnectedProperties\SL4\Debug
+copy /Y Help\current\Help\ConnectedProperties.xml Binaries\ConnectedProperties\SL4\Debug
 
 copy /Y "Source\ConnectedProperties (SL)\bin\Release\ConnectedProperties.dll" Binaries\ConnectedProperties\SL4
 copy /Y "Source\ConnectedProperties (SL)\bin\Release\ConnectedProperties.pdb" Binaries\ConnectedProperties\SL4
-copy /Y "Source\ConnectedProperties (SL)\bin\Release\ConnectedProperties.xml" Binaries\ConnectedProperties\SL4
+copy /Y Help\current\Help\ConnectedProperties.xml Binaries\ConnectedProperties\SL4
 copy /Y "Source\ConnectedProperties (SL)\bin\Release\CodeContracts\ConnectedProperties.Contracts.dll" Binaries\ConnectedProperties\SL4\CodeContracts
 copy /Y "Source\ConnectedProperties (SL)\bin\Release\CodeContracts\ConnectedProperties.Contracts.pdb" Binaries\ConnectedProperties\SL4\CodeContracts
 
