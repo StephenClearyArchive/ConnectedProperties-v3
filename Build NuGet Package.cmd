61a7591acf2af58848bb1f13e3f932f8e2898ab3
Stephen Cleary <StephenCleary@users.noreply.github.com>
2011-05-26 10:44:20 -0400

    Updated NuGet and updated NuGet package to remove Code Contracts and add source/symbol debugging.

--- Build NuGet Package.cmd
+++ Build NuGet Package.cmd
@@ -1,5 +1,9 @@
 @echo off
 if not exist Binaries mkdir Binaries
 cd Source
-..\Util\nuget.exe p Nito.ConnectedProperties.nuspec -o ..\Binaries
+..\Util\nuget.exe pack Nito.ConnectedProperties.nuspec -o ..\Binaries
+@echo
+@echo Please rename the .nupkg file to .symbols.nupkg
+pause
+..\Util\nuget.exe pack Nito.ConnectedProperties.nuspec -o ..\Binaries -Exclude **\*.pdb -Exclude **\*.cs
 pause
--- Source/ConnectedProperties/Properties/SharedAssemblyInfo.cs
+++ Source/ConnectedProperties/Properties/SharedAssemblyInfo.cs
@@ -6,6 +6,7 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyDescription("An API to \"attach\" properties to (most) .NET objects at runtime.")]
 [assembly: AssemblyCompany("Nito Programs")]
 [assembly: AssemblyProduct("Nito.ConnectedProperties")]
 [assembly: AssemblyCopyright("Copyright © Nito Programs 2011")]
@@ -13,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
