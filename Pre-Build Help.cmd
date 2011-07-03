c5455a45492ce8def8b02febbce3be47031f23e1
Stephen Cleary <StephenCleary@users.noreply.github.com>
2011-07-02 21:38:00 -0400

    Upgraded to latest version of SHFB.

--- Pre-Build Help.cmd
+++ Pre-Build Help.cmd
@@ -6,6 +6,6 @@ copy /Y ..\..\Source\ConnectedProperties\bin\Release\ConnectedProperties.dll .
 copy /Y ..\..\Source\ConnectedProperties\bin\Release\ConnectedProperties.xml .
 copy /Y ..\Topics\*.aml .
 
-..\..\Util\FixXmlDocumentation.exe --pre
+..\..\Util\FixXmlDocumentation.exe
 
 pause
