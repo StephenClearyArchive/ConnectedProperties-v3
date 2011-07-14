82df7ffa009bd80567560fda997577b6fae852d8
Stephen Cleary <StephenCleary@users.noreply.github.com>
2011-07-13 23:44:01 -0400

    Fixed NuGet package to include xmldoc files.

--- Build NuGet Package.cmd
+++ Build NuGet Package.cmd
@@ -1,8 +1,8 @@
 @echo off
 if not exist Binaries mkdir Binaries
 cd Source
-..\Util\nuget.exe pack Nito.ConnectedProperties.nuspec -o ..\Binaries
+..\Util\nuget.exe pack Nito.ConnectedProperties.nuspec -o ..\Binaries -BasePath ..
 @echo Please rename the .nupkg file to .symbols.nupkg
 pause
-..\Util\nuget.exe pack Nito.ConnectedProperties.nuspec -o ..\Binaries -Exclude **\*.pdb -Exclude **\*.cs
+..\Util\nuget.exe pack Nito.ConnectedProperties.nuspec -o ..\Binaries -Exclude **\*.pdb -Exclude **\*.cs -BasePath ..
 pause
