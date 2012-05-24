a361720d45d20bacf04eea486d1295c00f827a49
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-05-24 18:16:06 -0400

    Removed strong-name signing and bumped next version to 2.0.0 instead of 1.4.0.

--- Source/ConnectedProperties/Properties/SharedAssemblyInfo.cs
+++ Source/ConnectedProperties/Properties/SharedAssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
