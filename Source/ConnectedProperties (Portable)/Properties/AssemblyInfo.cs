f555ee3fd10faa9a6cccc39a4fb9f302cd7e0812
Stephen Cleary <Stephen Cleary@localhost>
2012-05-24 17:47:05 -0400

    Added a portable library build, but had to use "AsObject" since dynamic isn't supported.

---
+++ Source/ConnectedProperties (Portable)/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Nito Programs">
+//     Copyright (c) 2011 Nito Programs.
+// </copyright>
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Connected property support for .NET 4.0 Platform Update 3 and Metro")]
