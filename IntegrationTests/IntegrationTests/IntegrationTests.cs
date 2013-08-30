7b6b93c6cc7a84ff606673e9d1661debe9908e4d
Stephen Cleary <StephenCleary@users.noreply.github.com>
2013-08-29 23:03:31 -0400

    Removed .NET client profile unit tests.

--- IntegrationTests/IntegrationTests/IntegrationTests.cs
+++ IntegrationTests/IntegrationTests/IntegrationTests.cs
@@ -11,24 +11,6 @@ namespace IntegrationTests
     public class IntegrationTests
     {
         [TestMethod]
-        public void Net4Client_WithCC_HasCCExceptionType()
-        {
-            var assembly = Assembly.LoadFrom(@"..\..\..\Net 4 Client CC\bin\Debug\Net 4 Client CC.exe");
-            var method = assembly.GetType("IntegrationTest.Program").GetMethod("PreconditionViolationExceptionType", BindingFlags.Static | BindingFlags.Public);
-            string result = (string)method.Invoke(null, null);
-            Assert.IsTrue(result.Contains("System.Diagnostics.Contracts"));
-        }
-
-        [TestMethod]
-        public void Net4Client_WithoutCC_HasCCExceptionType()
-        {
-            var assembly = Assembly.LoadFrom(@"..\..\..\Net 4 Client NoCC\bin\Debug\Net 4 Client NoCC.exe");
-            var method = assembly.GetType("IntegrationTest.Program").GetMethod("PreconditionViolationExceptionType", BindingFlags.Static | BindingFlags.Public);
-            string result = (string)method.Invoke(null, null);
-            Assert.IsTrue(result.Contains("System.Diagnostics.Contracts"));
-        }
-
-        [TestMethod]
         public void Net4Full_WithCC_HasCCExceptionType()
         {
             var assembly = Assembly.LoadFrom(@"..\..\..\Net 4 Full CC\bin\Debug\Net 4 Full CC.exe");
