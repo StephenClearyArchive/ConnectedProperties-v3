c62b380e96063c6585dea46f47cba4b48a8b8652
Stephen Cleary <StephenCleary@users.noreply.github.com>
2013-08-29 22:51:59 -0400

    Changed NuGet package name from ConnectedProperties to Nito.ConnectedProperties.

--- Source/Build.cmd
+++ Source/Build.cmd
@@ -1,4 +1,5 @@
 @echo off
 if not exist ..\Binaries mkdir ..\Binaries
 devenv Nito.ConnectedProperties.sln /rebuild Release
+..\Util\nuget.exe pack Nito.ConnectedProperties.old.nuspec -o ..\Binaries
 ..\Util\nuget.exe pack -sym Nito.ConnectedProperties.nuspec -o ..\Binaries
