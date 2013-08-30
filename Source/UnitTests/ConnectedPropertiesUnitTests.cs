d5e3abeea1c007a1b6142fa5fab6c7688ba2605e
Stephen Cleary <StephenCleary@users.noreply.github.com>
2013-08-29 22:36:30 -0400

    Fleshed out unit tests.

---
+++ Source/UnitTests/ConnectedPropertiesUnitTests.cs
@@ -0,0 +1,309 @@
+﻿// Copyright (c) 2011-2013 Nito Programs.
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nito.ConnectedProperties;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ConnectedPropertiesUnitTests
+    {
+        [TestMethod]
+        public void TryConnect_WhenDisconnected_ReturnsTrueAndSetsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            Assert.IsTrue(PropertyConnector.Default.Get(carrier, name).TryConnect(17));
+            Assert.AreEqual(17, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void TryConnect_WhenConnected_ReturnsFalseAndDoesNotSetValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            Assert.IsFalse(PropertyConnector.Default.Get(carrier, name).TryConnect(17));
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void Connect_WhenDisconnected_SetsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Connect(13);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void Connect_WhenConnected_ThrowsAndDoesNotModifyValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Connect(13);
+            var property = PropertyConnector.Default.Get(carrier, name);
+            AssertEx.Throws<InvalidOperationException>(() => property.Connect(17));
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void TryDisconnect_WhenConnected_ReturnsTrueAndDisconnects()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            Assert.IsTrue(PropertyConnector.Default.Get(carrier, name).TryDisconnect());
+            Assert.IsFalse(PropertyConnector.Default.Get(carrier, name).TryDisconnect());
+        }
+
+        [TestMethod]
+        public void TryDisconnect_WhenDisconnected_ReturnsFalse()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            Assert.IsFalse(PropertyConnector.Default.Get(carrier, name).TryDisconnect());
+        }
+
+        [TestMethod]
+        public void Disconnect_WhenConnected_Disconnects()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            PropertyConnector.Default.Get(carrier, name).Disconnect();
+            Assert.IsFalse(PropertyConnector.Default.Get(carrier, name).TryDisconnect());
+        }
+
+        [TestMethod]
+        public void Disconnect_WhenDisconnected_Throws()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            var property = PropertyConnector.Default.Get(carrier, name);
+            AssertEx.Throws<InvalidOperationException>(property.Disconnect);
+        }
+
+        [TestMethod]
+        public void TryGet_WhenConnected_ReturnsTrueAndValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            dynamic value;
+            Assert.IsTrue(PropertyConnector.Default.Get(carrier, name).TryGet(out value));
+            Assert.AreEqual(13, value);
+        }
+
+        [TestMethod]
+        public void Get_WhenConnected_ReturnsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void TryGet_WhenDisconnected_ReturnsFalse()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            dynamic value;
+            Assert.IsFalse(PropertyConnector.Default.Get(carrier, name).TryGet(out value));
+        }
+
+        [TestMethod]
+        public void Get_WhenDisconnected_Throws()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            var property = PropertyConnector.Default.Get(carrier, name);
+            AssertEx.Throws<InvalidOperationException>(() => property.Get());
+        }
+
+        [TestMethod]
+        public void GetOrConnect_WhenDisconnected_SetsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            var result = PropertyConnector.Default.Get(carrier, name).GetOrConnect(13);
+            Assert.AreEqual(13, result);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void GetOrConnect_WhenConnected_ReturnsExistingValueAndDoesNotModifyValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(17);
+            var result = PropertyConnector.Default.Get(carrier, name).GetOrConnect(13);
+            Assert.AreEqual(17, result);
+            Assert.AreEqual(17, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void Set_WhenDisconnected_SetsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(17);
+            Assert.AreEqual(17, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void Set_WhenConnected_OverwritesExistingValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(17);
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void ConnectOrUpdate_WhenDisconnected_SetsAndReturnsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            var result = PropertyConnector.Default.Get(carrier, name).Cast<int>().ConnectOrUpdate(13, x => x + 2);
+            Assert.AreEqual(13, result);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void ConnectOrUpdate_WhenConnected_UpdatesAndReturnsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            var result = PropertyConnector.Default.Get(carrier, name).Cast<int>().ConnectOrUpdate(13, x => x + 2);
+            Assert.AreEqual(15, result);
+            Assert.AreEqual(15, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void TryUpdate_WhenDisconnected_ReturnsFalseAndDoesNotSetValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            var result = PropertyConnector.Default.Get(carrier, name).TryUpdate(19, 17);
+            Assert.IsFalse(result);
+            Assert.IsFalse(PropertyConnector.Default.Get(carrier, name).TryDisconnect());
+        }
+
+        [TestMethod]
+        public void TryUpdate_WhenConnectedWithNonmatchingValue_ReturnsFalseAndDoesNotSetValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            var result = PropertyConnector.Default.Get(carrier, name).TryUpdate(19, 17);
+            Assert.IsFalse(result);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void TryUpdate_WhenConnectedWithMatchingValue_ReturnsTrueAndSetsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            var result = PropertyConnector.Default.Get(carrier, name).TryUpdate(19, 13);
+            Assert.IsTrue(result);
+            Assert.AreEqual(19, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void GetOrCreate_WhenConnected_GetsValueAndDoesNotSetValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            var result = PropertyConnector.Default.Get(carrier, name).GetOrCreate(() => 17);
+            Assert.AreEqual(13, result);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void GetOrCreate_WhenDisconnected_SetsAndReturnsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            var result = PropertyConnector.Default.Get(carrier, name).GetOrCreate(() => 17);
+            Assert.AreEqual(17, result);
+            Assert.AreEqual(17, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void CreateOrUpdate_WhenDisconnected_SetsAndReturnsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            var result = PropertyConnector.Default.Get(carrier, name).CreateOrUpdate(() => 13, x => x + 2);
+            Assert.AreEqual(13, result);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void CreateOrUpdate_WhenConnected_UpdatesAndReturnsValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(17);
+            var result = PropertyConnector.Default.Get(carrier, name).CreateOrUpdate(() => 13, x => x + 2);
+            Assert.AreEqual(19, result);
+            Assert.AreEqual(19, PropertyConnector.Default.Get(carrier, name).Get());
+        }
+
+        [TestMethod]
+        public void Cast_ReferenesSamePropertyValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            Assert.AreEqual(13, PropertyConnector.Default.Get(carrier, name).Cast<int>().Get());
+        }
+
+        [TestMethod]
+        public void Cast_ToInvalidType_ThrowsWhenUsed()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            var property = PropertyConnector.Default.Get(carrier, name).Cast<string>();
+            AssertEx.Throws<InvalidCastException>(() => property.Get());
+        }
+
+        [TestMethod]
+        public void TryCastFrom_ReferencesSamePropertyValue()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            Assert.AreEqual(13, ConnectibleProperty<int>.TryCastFrom(PropertyConnector.Default.Get(carrier, name)).Get());
+        }
+
+        [TestMethod]
+        public void TryCastFrom_ToInvalidType_ThrowsWhenUsed()
+        {
+            var carrier = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name).Set(13);
+            var property = ConnectibleProperty<string>.TryCastFrom(PropertyConnector.Default.Get(carrier, name));
+            AssertEx.Throws<InvalidCastException>(() => property.Get());
+        }
+
+        [TestMethod]
+        public void TryCastFrom_WithNullSource_ReturnsNull()
+        {
+            ConnectibleProperty<dynamic> source = null;
+            Assert.IsNull(ConnectibleProperty<string>.TryCastFrom(source));
+        }
+    }
+}
---
+++ Source/UnitTests/PropertyConnectorUnitTests.cs
@@ -0,0 +1,194 @@
+﻿// Copyright (c) 2011-2013 Nito Programs.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nito.ConnectedProperties;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PropertyConnectorUnitTests
+    {
+        private struct tag1 { };
+
+        [TestMethod]
+        public void TryGet_ForValueTypeCarrier_ReturnsNull()
+        {
+            var carrier = 13;
+            var name = Guid.NewGuid().ToString("N");
+            Assert.IsNull(PropertyConnector.Default.TryGet(carrier, name));
+        }
+
+        [TestMethod]
+        public void TryGet_ForStringCarrier_ReturnsNull()
+        {
+            var carrier = "Hi";
+            var name = Guid.NewGuid().ToString("N");
+            Assert.IsNull(PropertyConnector.Default.TryGet(carrier, name));
+        }
+
+        [TestMethod]
+        public void TryGet_ForValueTypeCarrierWithoutValidation_DoesNotReturnNull()
+        {
+            // Please note: this is an extremely dangerous example! Do not use in real-world code!
+            var carrier = 13;
+            var name = Guid.NewGuid().ToString("N");
+            Assert.IsNotNull(PropertyConnector.Default.TryGet(carrier, name, bypassValidation: true));
+        }
+
+        [TestMethod]
+        public void TryGet_ForStringCarrierWithoutValidation_DoesNotReturnNull()
+        {
+            // Please note: this is an extremely dangerous example! Do not use in real-world code!
+            var carrier = "Hi";
+            var name = Guid.NewGuid().ToString("N");
+            Assert.IsNotNull(PropertyConnector.Default.TryGet(carrier, name, bypassValidation: true));
+        }
+
+        [TestMethod]
+        public void Get_ForValueTypeCarrier_Throws()
+        {
+            var carrier = 13;
+            var name = Guid.NewGuid().ToString("N");
+            var connector = PropertyConnector.Default;
+            AssertEx.Throws<InvalidOperationException>(() => connector.Get(carrier, name));
+        }
+
+        [TestMethod]
+        public void Get_ForStringCarrier_Throws()
+        {
+            var carrier = "Hi";
+            var name = Guid.NewGuid().ToString("N");
+            var connector = PropertyConnector.Default;
+            AssertEx.Throws<InvalidOperationException>(() => connector.Get(carrier, name));
+        }
+
+        [TestMethod]
+        public void Get_ForValueTypeCarrierWithoutValidation_DoesNotThrow()
+        {
+            // Please note: this is an extremely dangerous example! Do not use in real-world code!
+            var carrier = 13;
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name, bypassValidation: true);
+        }
+
+        [TestMethod]
+        public void Get_ForStringCarrierWithoutValidation_DoesNotThrow()
+        {
+            // Please note: this is a highly dangerous example! Do not use in real-world code unless you know for-sure what you're doing!
+            var carrier = "Hi";
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier, name, bypassValidation: true);
+        }
+
+        [TestMethod]
+        public void CopyAll_ForValueTypeSourceCarrier_Throws()
+        {
+            var carrier1 = 13;
+            var carrier2 = new object();
+            var connector = PropertyConnector.Default;
+            AssertEx.Throws<InvalidOperationException>(() => connector.CopyAll(carrier1, carrier2));
+        }
+
+        [TestMethod]
+        public void CopyAll_ForValueTypeTargetCarrier_Throws()
+        {
+            var carrier1 = new object();
+            var carrier2 = 13;
+            var connector = PropertyConnector.Default;
+            AssertEx.Throws<InvalidOperationException>(() => connector.CopyAll(carrier1, carrier2));
+        }
+
+        [TestMethod]
+        public void CopyAll_ForValueTypeCarriersWithoutValidation_DoesNotThrow()
+        {
+            var carrier1 = 13;
+            var carrier2 = 17;
+            PropertyConnector.Default.CopyAll(carrier1, carrier2, true);
+        }
+
+        [TestMethod]
+        public void CopyAll_WithNamedTaggedAndNamedTagged_CopiesAllValues()
+        {
+            var carrier1 = new object();
+            var carrier2 = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier1, name).Set(5);
+            PropertyConnector.Default.Get<tag1>(carrier1).Set(7);
+            PropertyConnector.Default.Get<tag1>(carrier1, name).Set(11);
+            PropertyConnector.Default.CopyAll(carrier1, carrier2);
+            Assert.AreEqual(5, PropertyConnector.Default.Get(carrier2, name).Get());
+            Assert.AreEqual(7, PropertyConnector.Default.Get<tag1>(carrier2).Get());
+            Assert.AreEqual(11, PropertyConnector.Default.Get<tag1>(carrier2, name).Get());
+        }
+
+        [TestMethod]
+        public void CopyAll_WhenTargetAlreadyHasValue_OverwritesTargetValue()
+        {
+            var carrier1 = new object();
+            var carrier2 = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier1, name).Set(5);
+            PropertyConnector.Default.Get(carrier2, name).Set(7);
+            PropertyConnector.Default.CopyAll(carrier1, carrier2);
+            Assert.AreEqual(5, PropertyConnector.Default.Get(carrier2, name).Get());
+        }
+
+        [TestMethod]
+        public void TryCopyAll_ForValueTypeSourceCarrier_ReturnsFalse()
+        {
+            var carrier1 = 13;
+            var carrier2 = new object();
+            var result = PropertyConnector.Default.TryCopyAll(carrier1, carrier2);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TryCopyAll_ForValueTypeTargetCarrier_ReturnsFalse()
+        {
+            var carrier1 = new object();
+            var carrier2 = 13;
+            var result = PropertyConnector.Default.TryCopyAll(carrier1, carrier2);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TryCopyAll_ForValueTypeCarriersWithoutValidation_ReturnsTrue()
+        {
+            var carrier1 = 13;
+            var carrier2 = 17;
+            var result = PropertyConnector.Default.TryCopyAll(carrier1, carrier2, true);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TryCopyAll_WithNamedTaggedAndNamedTagged_CopiesAllValues()
+        {
+            var carrier1 = new object();
+            var carrier2 = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier1, name).Set(5);
+            PropertyConnector.Default.Get<tag1>(carrier1).Set(7);
+            PropertyConnector.Default.Get<tag1>(carrier1, name).Set(11);
+            PropertyConnector.Default.TryCopyAll(carrier1, carrier2);
+            Assert.AreEqual(5, PropertyConnector.Default.Get(carrier2, name).Get());
+            Assert.AreEqual(7, PropertyConnector.Default.Get<tag1>(carrier2).Get());
+            Assert.AreEqual(11, PropertyConnector.Default.Get<tag1>(carrier2, name).Get());
+        }
+
+        [TestMethod]
+        public void TryCopyAll_WhenTargetAlreadyHasValue_OverwritesTargetValue()
+        {
+            var carrier1 = new object();
+            var carrier2 = new object();
+            var name = Guid.NewGuid().ToString("N");
+            PropertyConnector.Default.Get(carrier1, name).Set(5);
+            PropertyConnector.Default.Get(carrier2, name).Set(7);
+            PropertyConnector.Default.TryCopyAll(carrier1, carrier2);
+            Assert.AreEqual(5, PropertyConnector.Default.Get(carrier2, name).Get());
+        }
+    }
+}
